372136f6018adeab214b76561f56e29d425b45e7
Jonathas Leontino Medina <emaildojonathas@gmail.com>
2023-09-28 18:50:18 -0400

    -

---
+++ app/src/main/java/com/example/provan1_23_2/AtividadeComplementar.java
@@ -0,0 +1,68 @@
+package com.example.provan1_23_2;
+
+class AtividadeComplementar {
+    String nomeEstudante;
+    String emailInstitucional;
+    String descricaoAtiv;
+    String horasPret;
+    String categoriaAtiv;
+
+    public AtividadeComplementar(String nomeEstudante, String emailInstitucional, String descricaoAtiv, String horasPret, String categoriaAtiv) {
+        this.nomeEstudante = nomeEstudante;
+        this.emailInstitucional = emailInstitucional;
+        this.descricaoAtiv = descricaoAtiv;
+        this.horasPret = horasPret;
+        this.categoriaAtiv = categoriaAtiv;
+    }
+
+    public String getNomeEstudante() {
+        return nomeEstudante;
+    }
+
+    public void setNomeEstudante(String nomeEstudante) {
+        this.nomeEstudante = nomeEstudante;
+    }
+
+    public String getEmailInstitucional() {
+        return emailInstitucional;
+    }
+
+    public void setEmailInstitucional(String emailInstitucional) {
+        this.emailInstitucional = emailInstitucional;
+    }
+
+    public String getDescricaoAtiv() {
+        return descricaoAtiv;
+    }
+
+    public void setDescricaoAtiv(String descricaoAtiv) {
+        this.descricaoAtiv = descricaoAtiv;
+    }
+
+    public String getHorasPret() {
+        return horasPret;
+    }
+
+    public void setHorasPret(String horasPret) {
+        this.horasPret = horasPret;
+    }
+
+    public String getCategoriaAtiv() {
+        return categoriaAtiv;
+    }
+
+    public void setCategoriaAtiv(String categoriaAtiv) {
+        this.categoriaAtiv = categoriaAtiv;
+    }
+
+    @Override
+    public String toString() {
+        return "AtividadeComplementar{" +
+                "nomeEstudante='" + nomeEstudante + '\'' +
+                ", emailInstitucional='" + emailInstitucional + '\'' +
+                ", descricaoAtiv='" + descricaoAtiv + '\'' +
+                ", horasPret='" + horasPret + '\'' +
+                ", categoriaAtiv='" + categoriaAtiv + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/com/example/provan1_23_2/MainActivityCadastro.java
+++ app/src/main/java/com/example/provan1_23_2/MainActivityCadastro.java
@@ -3,8 +3,13 @@ package com.example.provan1_23_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivityCadastro extends AppCompatActivity {
+
+    EditText edNome, edEmail, edDescricao, edHoras;
+    Button btSalvar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
